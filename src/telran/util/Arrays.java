9baabb448935c99344f7644ae56ba068ba003aaf
Arseniy Gorbikov <gorbikovarseniy@gmail.com>
2024-04-09 07:14:01 +0300

    CW #14

--- src/telran/util/Arrays.java
+++ src/telran/util/Arrays.java
@@ -4,99 +4,103 @@ import java.util.Comparator;
 import java.util.function.Predicate;
 
 public class Arrays {
-    public static <T> int indexOf(T[] array, T element) {
+    public static  <T> int indexOf(T[] array, T element) {
         int index = 0;
-        while (index < array.length && !equals(array[index], element)) {
+        while(index < array.length && !equals(array[index], element)) {
             index++;
         }
-
         return index == array.length ? -1 : index;
     }
 
     private static <T> boolean equals(T elem1, T elem2) {
+
         return elem1 == null ? elem1 == elem2 : elem1.equals(elem2);
     }
-
-    public static <T> T min(T[] array, Comparator<T> comparator) {
-        T res = null;
+    public static <T> T min(T[] array, Comparator<T> comp) {
+        T res=null;
         if (array != null && array.length > 0) {
             res = array[0];
-            for (int i = 1; i < array.length; i++) {
-                if (comparator.compare(res, array[i]) > 0) {
+            for(int i = 1; i < array.length; i++) {
+                if (comp.compare(res, array[i]) > 0) {
                     res = array[i];
                 }
             }
         }
         return res;
-    }
 
-    public static <T> void bubbleSort(T[] array, Comparator<T> comparator) {
-        int n = array.length;
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 1; i < n; i++) {
-                if (comparator.compare(array[i - 1], array[i]) > 0) {
-                    T temp = array[i - 1];
-                    array[i - 1] = array[i];
-                    array[i] = temp;
-                    swapped = true;
+    }
+    public static <T> void bubbleSort(T[] array, Comparator<T> comp) {
+        boolean isUnsorted = true;
+        int length = array.length;
+        while(isUnsorted) {
+            length--;
+            isUnsorted = false;
+            for(int i = 0; i < length; i++) {
+                if(comp.compare(array[i], array[i + 1]) > 0) {
+                    swap(array, i, i + 1);
+                    isUnsorted = true;
                 }
             }
-            n--;
-        } while (swapped);
+        }
     }
-
-    public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+    public static <T> void bubbleSort(T[] array) {
         //TODO
-        int leftIndex = 0;
-        int rightIndex = array.length - 1;
-        int res = -1;
-        boolean indexNotFound = true;
-        while (leftIndex <= rightIndex && indexNotFound) {
-            int middleIndex = (leftIndex + rightIndex) >>> 1;
-            int comparing = comp.compare(array[middleIndex], key);
-            if (comparing == 0) {
-                res = middleIndex;
-                indexNotFound = false;
-            } else if (comparing > 0) {
-                rightIndex = middleIndex - 1;
-            } else {
-                leftIndex = middleIndex + 1;
-            }
-
-        }
-        if (indexNotFound){
-            res = -(leftIndex + 1);
-        }
-        return res;
+        //research: write the code based on the existing one of bubbleSort with comparator
     }
 
+    private static <T> void swap(T[] array, int i, int j) {
+        T tmp = array[i];
+        array[i] = array[j];
+        array[j] =tmp;
+
+    }
+    public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+        //left index = 0
+        //right index = array.length - 1
+        //middle (left + right) / 2
+        //left part - left index, right index = middle - 1
+        //right part - left index = middle + 1, right index
+        //while left <= right
+        //returns exactly what the standard binarySearch does
+        //if there are several equaled elements no guarantee that
+        // being returned index is the one to the first occurrence
+        int left = 0;
+        int right = array.length - 1;
+        int middle = (left + right) / 2;
+        int res = -1;
+        while(left <= right && (res = comp.compare(key, array[middle])) != 0) {
+            if (res > 0) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+            middle = (left + right) / 2;
+        }
+        return left > right ? -(left + 1) : middle;
+    }
     public static <T> T[] search(T[] array, Predicate<T> predicate) {
         //Impossible to allocate memory for generic array
         //Only Arrays.copyOf may be used
         T[] arResult = java.util.Arrays.copyOf(array, array.length);
         int index = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (predicate.test(array[i])) {
+        for(int i = 0; i < array.length; i++) {
+            if(predicate.test(array[i])) {
                 arResult[index++] = array[i];
             }
         }
-        return java.util.Arrays.copyOf(arResult, index);
+        return java.util.Arrays.copyOf(arResult,index);
     }
-
     public static <T> T[] removeIf(T[] array, Predicate<T> predicate) {
-        /* implementation of the method, without repeating code */
-        return search(array, a -> !predicate.test(a));
-
-        /* --------- Duplicate method search() with invert condition --------- */
-//        T[] arResult = java.util.Arrays.copyOf(array, array.length);
-//        int index = 0;
-//        for (int i = 0; i < array.length; i++) {
-//            if (!predicate.test(array[i])) {
-//                arResult[index++] = array[i];
-//            }
-//        }
-//        return java.util.Arrays.copyOf(arResult, index);
+        //removes all elements of array matching a given predicate
+        //returns new array with no elements matching a given predicate
+        return search(array, predicate.negate());
+    }
+    public static <T> T[] add (T[] array, T element) {
+        T[] result = java.util.Arrays.copyOf(array, array.length + 1);
+        result[array.length] = element;
+        return result;
+    }
+    public static <T> T[] copy(T[] array) {
+        return java.util.Arrays.copyOf(array, array.length);
     }
 }
--- src/telran/util/test/ArraysTests.java
+++ src/telran/util/test/ArraysTests.java
@@ -3,6 +3,7 @@ package telran.util.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,6 @@ import telran.util.Arrays;
 
 class ArraysTests {
     Integer[] numbers = {100, -3, 23, 4, 8, 41, 56, -7};
-    Integer[] numbersSorted = {1, 5, 9, 14, 17, 43, 64, 79, 98};
     String[] strings = {"abc", "lmn", "123", null, "a"};
     String[] stringsMin = {"abc", "lmn", "123",  "y"};
     @Test
@@ -25,8 +25,7 @@ class ArraysTests {
         Comparator<String> compLength = (s1, s2) -> s1.length() - s2.length();
         assertEquals("y", Arrays.min(stringsMin,
                 compLength));
-        Comparator<String> compNative = (s1, s2) -> s1.compareTo(s2);
-        assertEquals("123", Arrays.min(stringsMin, compNative));
+        assertEquals("123", Arrays.min(stringsMin, (s1, s2) -> s1.compareTo(s2)));
     }
     @Test
     void bubbleSortTest() {
@@ -70,19 +69,83 @@ class ArraysTests {
     }
     @Test
     void binarySearchTest() {
-        assertEquals(-2, Arrays.binarySearch(numbersSorted, 3, Integer::compare));
-        assertEquals(-2, java.util.Arrays.binarySearch(numbersSorted, 3, Integer::compare));
-        assertEquals(4, Arrays.binarySearch(numbersSorted, 17, Integer::compare));
-        assertEquals(4, java.util.Arrays.binarySearch(numbersSorted, 17, Integer::compare));
-        assertEquals(-10, Arrays.binarySearch(numbersSorted, 99, Integer::compare));
-        assertEquals(-10, java.util.Arrays.binarySearch(numbersSorted, 99, Integer::compare));
+        Integer[] sortedNumbers = {10, 20, 30, 40, 50};
+        Comparator<Integer> comp = Integer::compare;
+        assertEquals(0, Arrays.binarySearch(sortedNumbers, 10, comp));
+        assertEquals(4, Arrays.binarySearch(sortedNumbers, 50, comp));
+        assertEquals(2, Arrays.binarySearch(sortedNumbers, 30, comp));
+        assertEquals(-1, Arrays.binarySearch(sortedNumbers, 5, comp));
+        assertEquals(-4, Arrays.binarySearch(sortedNumbers, 35, comp));
+        assertEquals(-6, Arrays.binarySearch(sortedNumbers, 55, comp));
     }
     @Test
     void removeIfTest() {
-        Integer[] originalArray = {111, 222, 333, 444, 555, 777, 888, 9999};
-        Integer[] expectedArrayWithoutOddNumbers = {222, 444, 888};
-        Integer[] expectedArrayWithoutEvenNumbers = {111,333,555,777,9999};
-        assertArrayEquals(expectedArrayWithoutEvenNumbers, Arrays.removeIf(originalArray, a -> a %2 ==0));
-        assertArrayEquals(expectedArrayWithoutOddNumbers, Arrays.removeIf(originalArray, a -> a %2 !=0));
+        Integer[] expectedEven = {100, 4, 8,  56};
+        assertArrayEquals(expectedEven, Arrays.removeIf(numbers,
+                a -> a % 2 != 0));
+        Integer[] expectedNegative = {-3,-7};
+        assertArrayEquals(expectedNegative, Arrays.removeIf(numbers,
+                a -> a > 0));
     }
+    @Test
+    void addTest() {
+        Integer[] expected = {100, -3, 23, 4, 8, 41, 56, -7, 150};
+        Integer[] actual = Arrays.add(numbers, 150);
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void personsSortTest() {
+        Person prs1 = new Person(123, 1985);
+        Person prs2 = new Person(120, 2000);
+        Person prs3 = new Person(128, 1999);
+        Person[] persons = {
+                prs1, prs2, prs3
+        };
+        Arrays.bubbleSort(persons);
+        Person[] expected = {new Person(120, 2000),
+                new Person(123, 1985),
+                new Person(128, 1999)};
+        Person[] expectedAge = {
+                new Person(120, 2000),
+                new Person(128, 1999),
+                new Person(123, 1985)
+        };
+
+        assertArrayEquals(expected, persons);
+        Arrays.bubbleSort(persons,
+                (p1, p2) -> Integer.compare(p2.birthYear, p1.birthYear));
+
+        assertArrayEquals(expectedAge, persons);
+    }
+
+}
+class Person implements Comparable<Person>{
+    long id;
+    int birthYear;
+    public Person(long id, int birthYear) {
+        this.id = id;
+        this.birthYear = birthYear;
+    }
+    @Override
+    public int compareTo(Person o) {
+
+        return Long.compare(id, o.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(birthYear, id);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        return birthYear == other.birthYear && id == other.id;
+    }
+
+
 }
